90461c01e6dd861954521b52091f8889ef35b592
201411183 김혜지 <khji1210@naver.com>
2017-04-17 14:50:02 +0900

    Add files via upload

---
+++ src/headfirst/factory/pizzas/ClamPizza.java
@@ -0,0 +1,11 @@
+package headfirst.factory.pizzas;
+
+public class ClamPizza extends Pizza{
+  public ClamPizza(){
+    name="Clam Pizza";
+    dough="Regular Crust";
+    sauce="Marinara Pizza Sauce";
+    toppings.add("Fresh Clam");
+    
+  }
+}
---
+++ src/headfirst/factory/pizzas/PotatoPizza.java
@@ -0,0 +1,11 @@
+package headfirst.factory.pizzas;
+
+public class PotatoPizza extends Pizza{
+  public PotatoPizza(){
+    name="Potato Pizza";
+    dough="Regular Crust";
+    sauce="Tomato Sauce";
+    toppings.add("Cream Cheese");
+    toppings.add("Slice Potato");
+   }
+}
